726dbf82489b9ae7e7d8f554aa9b615b4ed42c30
Andrew Thomas Agustinus <99781268+AndrewTA21@users.noreply.github.com>
2022-10-09 15:12:19 +0700

    login signup

--- view/login.php
+++ view/login.php
@@ -1,28 +1,63 @@
-<div class="modal-dialog">
-    <div class="modal-content">
-    <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">LOGIN</h5>
-    </div>
-    <div class="modal-body">
-    <?php if($error != false) : ?>
-        <div class="alert alert-danger" role="alert">
-            <?= $error; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Login Page</title>
+    <style>
+        body {
+            background-color: #eeee;
+        }
+        .container{
+          background-color: white;
+        }
+    </style>
+  </head>
+  <body>    
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">LOGIN</h5>
+            </div>
+            <div class="modal-body">
+            <?php if($error != false) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= $error; ?>
+                </div>
+            <?php endif ?>
+            <form action="profile.php" method="post">
+                <div class="">
+                    <label for="username" class="form-label">Username</label>
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg></span>
+                    <input type="text" name="username" class="form-control" id="username" required>
+                </div>
+                <div class="mb-3">
+                    <label for="paaword" class="form-label">Password</label>
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-unlock-fill" viewBox="0 0 16 16">
+                    <path d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2z"/>
+                    </svg></span>
+                    <input type="password" name="password" class="form-control" id="paaword" required>
+                </div>
+                <div class="mb-3 text-center">
+                    <button class="btn btn-success" name="login">Login</button>
+                </div>
+            </form>
+            <center><a class="text-decoration-none" href="">Forgot Password</a></center>
+            <center><a class="text-center text-decoration-none" href="profile.php?signUp=true">Create an Account</a></center>
+            </div>
         </div>
-    <?php endif ?>
-    <form action="profile.php" method="post">
-        <div class="mb-3">
-            <label for="username" class="form-label">Username</label>
-            <input type="text" name="username" class="form-control" id="username">
-        </div>
-        <div class="mb-3">
-            <label for="paaword" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" id="paaword">
-        </div>
-        <button class="btn btn-success" name="login">Login</button>
-    </form>
-    <center><a class="text-decoration-none" href="">Forgot Password</a></center>
-    <center><a class="text-center text-decoration-none" href="profile.php?signUp=true">Create an Account</a></center>
-    
     </div>
-    </div>
-</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
